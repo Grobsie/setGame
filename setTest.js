ec8171c8ac34e80ae63b56bd771b7c8fcce5e10d
Grobsie <cruicer1@gmail.com>
2024-05-29 19:24:40 +0200

    changed to eventlistener

--- setTest.js
+++ setTest.js
@@ -1,25 +1,35 @@
-function combinations(cards, k) {
-    const results = [];
-    
-    function combine(startIndex, combination) {
-        if (combination.length === k) {
-            results.push([...combination]);
-            return;
-        }
-        
-        for (let i = startIndex; i < cards.length; i++) {
-            combination.push(cards[i]);
-            combine(i + 1, combination);
-            combination.pop();
-        }
-    }
-    
-    combine(0, []);
-    return results;
-}
+document.addEventListener('DOMContentLoaded', () => {
+    // Function that returns a promise which resolves after three button presses
+    function waitForThreeButtonPresses() {
+        return new Promise((resolve) => {
+            const buttonContainer = document.getElementById('buttonContainer');
+            let pressCount = 0;
 
-// Example usage:
-const cards = [1, 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
-console.log(cards[0]);
-cards[0] = [1,2,3,5];
-console.log(cards[0]);
+            function buttonClickHandler(event) {
+                if (event.target && event.target.classList.contains('myButton')) {
+                    pressCount++;
+                    console.log(`Button ${event.target.getAttribute('data-button-id')} was pressed.`);
+                    if (pressCount === 3) {
+                        // Remove the event listener once we reach three presses
+                        buttonContainer.removeEventListener('click', buttonClickHandler);
+                        resolve(); // Resolve the promise
+                    }
+                }
+            }
+
+            // Add event listener to the button container
+            buttonContainer.addEventListener('click', buttonClickHandler);
+        });
+    }
+
+    // Use an async function to wait for the button presses
+    async function main() {
+        console.log('Waiting for three button presses...');
+        await waitForThreeButtonPresses();
+        console.log('Three button presses detected. Continuing with the next part of the code...');
+        // Place your code here that should run after three button presses
+    }
+
+    // Start the main function
+    main();
+});
--- setscript.js
+++ setscript.js
@@ -1,155 +1,187 @@
-const deck = [];
-const hand = [[],[],[],[],[],[],[],[],[],[],[],[]];
-const selection = [];
+document.addEventListener('DOMContentLoaded', () => {
+    const startGameButton = document.getElementById('startGameButton');
+    const shuffleButton = document.getElementById('shuffleButton');
+    const buttonContainer = document.getElementById('handPane');
 
-function createDeck() {
-    const values = ["1","2","3"];
-    const shapes = ["triangle", "circle", "diamond"];
-    const shades = ["solid", "stripe", "open"];
-    const colors = ["red", "blue", "green"];
+    const deck = [];
+    const hand = [[],[],[],[],[],[],[],[],[],[],[],[]];
+    const selection = [];
 
-    for (let value in values) {
-        for (let shape in shapes){
-            for (let shade in shades){
-                for (let color in colors){
-                    deck.push([values[value],shapes[shape],shades[shade],colors[color]]);
+    function createDeck() {
+        const values = ["1","2","3"];
+        const shapes = ["triangle", "circle", "diamond"];
+        const shades = ["solid", "stripe", "open"];
+        const colors = ["red", "blue", "green"];
+
+        for (let value in values) {
+            for (let shape in shapes){
+                for (let shade in shades){
+                    for (let color in colors){
+                        deck.push([values[value],shapes[shape],shades[shade],colors[color]]);
+                    }
                 }
             }
         }
     }
-}
 
-function fillHand() {
-    let container = document.getElementById("handPane");
-    container.innerHTML = "";
-    console.log(hand[0]);
-    for (let i in hand){
-        if (hand[i].length === 0){
-            let x = Math.floor(Math.random() * deck.length);
-            hand[i] = deck[x];
-            deck.splice(x, 1);
+    function fillHand() {
+        console.log("running fill hand");
+        let container = document.getElementById("handPane");
+        container.innerHTML = "";
+        for (let i in hand){
+            if (hand[i].length === 0){
+                let x = Math.floor(Math.random() * deck.length);
+                hand[i] = deck[x];
+                deck.splice(x, 1);
 
-            var cardContainer = document.createElement("div");
-            for (let val = 0; val < hand[i][0];val++){
-                var shape = document.createElement("img");
-                shape.src = "img/"+hand[i][3]+"/"+hand[i][1] + hand[i][2]+".png"
-                shape.innerHTML = hand[i][1] + hand[i][2] + hand[i][3];
-                cardContainer.appendChild(shape);
-            }
-            //cardContainer.innerHTML = hand[i];
-            cardContainer.setAttribute("class", "shownCards");
-            cardContainer.setAttribute("id", i);
-            cardContainer.setAttribute("onclick", "addSelection(" + i + ")");
-            container.appendChild(cardContainer);
-        }
-
-    }	
-}
-
-function shuffle() {
-    for (let cardInHand in hand){
-        deck.push(hand[cardInHand]);
-        hand[cardInHand] = [];
-    }
-    fillHand();
-}
-
-function addSelection(arrIndex) {
-    let clickedCard = hand[arrIndex];
-    let cardDiv = document.getElementById(arrIndex);
-    if (selection.includes(clickedCard)) {
-        cardDiv.style.border = "1px white solid";
-        selection.splice(clickedCard, 1); 
-    } else {
-        cardDiv.style.border = "1px black solid";
-        console.log(clickedCard);
-        selection.push(clickedCard);
-        if (selection.length === 3){
-            console.log(isSet(selection));
-            let selectedCardDivs = document.getElementsByClassName("shownCards");
-            for (let indexOfElement in selectedCardDivs) {
-                console.log(selectedCardDivs);
-                selectedCardDivs[indexOfElement].style.border = "1px white solid";
-            }
-            if (isSet(selection) == true) {
-                removeSetFromHand(selection)
+                var cardContainer = document.createElement("div");
+                for (let val = 0; val < hand[i][0];val++){
+                    var shape = document.createElement("img");
+                    shape.src = "img/"+hand[i][3]+"/"+hand[i][1] + hand[i][2]+".png"
+                    shape.setAttribute("class", "shownCards");
+                    shape.setAttribute("id", i);
+                    cardContainer.appendChild(shape);
+                }
+                //cardContainer.innerHTML = hand[i];
+                //cardContainer.setAttribute("class", "shownCards");
+                //cardContainer.setAttribute("id", i);
+                //cardContainer.setAttribute("onclick", "addSelection(" + i + ")");
+                container.appendChild(cardContainer);
             }
 
+        }	
+    }
+
+    function shuffle() {
+        for (let cardInHand in hand){
+            deck.push(hand[cardInHand]);
+            hand[cardInHand] = [];
+        }
+        fillHand();
+    }
+
+    function waitForThreeSelections() {
+        console.log("now waiting for presses");
+
+        return new Promise((resolve) => {
+            function buttonClickHandler(event) {
+                if (event.target && event.target.classList.contains('shownCards')) {
+                    const buttonId = event.target.getAttribute('id');
+                    selection.push(buttonId);
+                    console.log("adding " + buttonId);
+
+                    if (selection.length >= 3) {
+                        buttonContainer.removeEventListener('click', buttonClickHandler);
+                        resolve(selection);
+                    }
+                }
+            }
+
+            buttonContainer.addEventListener('click', buttonClickHandler);
+        });
+    }
+
+    //obsolete, use code for borders maybe later
+    function addSelection(arrIndex) {
+        let clickedCard = hand[arrIndex];
+        let cardDiv = document.getElementById(arrIndex);
+        if (selection.includes(clickedCard)) {
+            cardDiv.style.border = "1px white solid";
+            selection.splice(clickedCard, 1); 
+        } else {
+            cardDiv.style.border = "1px black solid";
+            console.log(clickedCard);
+            selection.push(clickedCard);
+            if (selection.length === 3){
+                if (isSet(selection) == true) {
+                    console.log("Yay it is a set");
+                    removeSetFromHand(selection);
+                } else {
+                    console.log("Oh no it is not a set :(");
+                }
+
+                let selectedCardDivs = Array.from(document.getElementsByClassName("shownCards"));
+                for (let indexOfElement in selectedCardDivs) {
+                    selectedCardDivs[indexOfElement].style.border = "1px white solid";
+                }
+                selection.length = 0;
+            }
         }
     }
-}
 
-function removeSetFromHand(setArray) {
-    for (let indexOfSetCard in setArray) {
-        hand.splice(setArray[indexOfSetCard],1)
-    }
-}
-
-function isSet(selectedArray) {
-    let match = 0;
-    for (i = 0; i < 4; i++) {
-        if (selectedArray[0][i] == selectedArray[1][i] && selectedArray[0][i] == selectedArray[2][i]){
-            match++;
-        } else if (selectedArray[0][i] != selectedArray[1][i] && selectedArray[0][i] != selectedArray[2][i] && selectedArray[1][i] != selectedArray[2][i]) {
-            match++;
+    function removeSetFromHand(setArray) {
+        for (let indexOfSetCard in setArray) {
+            hand[hand.indexOf(setArray[indexOfSetCard])] = [];
         }
     }
-    selection.length = 0;
-    return (match === 4);
-}
 
-function combinations(cards) {
-    const results = [];
-    let possiblecombinations = 0;
-    
-    function combine(startIndex, combination) {
-        if (combination.length === 3) {
-            results.push([...combination]);
-            return;
+    function isSet(selectedArray) {
+        let match = 0;
+        for (i = 0; i < 4; i++) {
+            if (selectedArray[0][i] == selectedArray[1][i] && selectedArray[0][i] == selectedArray[2][i]){
+                match++;
+            } else if (selectedArray[0][i] != selectedArray[1][i] && selectedArray[0][i] != selectedArray[2][i] && selectedArray[1][i] != selectedArray[2][i]) {
+                match++;
+            }
+        }
+        return (match === 4);
+    }
+
+    function combinations(cards) {
+        const results = [];
+        let possiblecombinations = 0;
+        
+        function combine(startIndex, combination) {
+            if (combination.length === 3) {
+                results.push([...combination]);
+                return;
+            }
+            
+            for (let i = startIndex; i < cards.length; i++) {
+                combination.push(cards[i]);
+                combine(i + 1, combination);
+                combination.pop();
+            }
         }
         
-        for (let i = startIndex; i < cards.length; i++) {
-            combination.push(cards[i]);
-            combine(i + 1, combination);
-            combination.pop();
+        combine(0, []);
+        //check every entry if it is in fact a set
+        for (let set in results){
+            if (isSet(results[set]) == true) {
+                possiblecombinations++;
+            }
         }
+        return possiblecombinations;
     }
-    
-    combine(0, []);
-    //check every entry if it is in fact a set
-    for (let set in results){
-        if (isSet(results[set]) == true) {
-            possiblecombinations++;
+
+    async function startGame() {
+        deck.length = 0;
+        selection.length = 0;
+        console.log(hand);
+        createDeck();
+        //start while loop here
+        while (true) {
+            console.log("current hand is :");
+            console.log(hand);
+            fillHand();
+
+            document.getElementById("deckSize").innerHTML = "decksize: " + deck.length;
+            document.getElementById("log").innerHTML = "possible combinations: " + combinations(hand);
+            await waitForThreeSelections();
+            console.log("yay a selection has been made");
+            if (isSet(selection) == true) {
+                console.log("Yay it is a set");
+                removeSetFromHand(selection);
+            } else {
+                console.log("Oh no it is not a set :(");
+            }
+            selection.length = 0;               
         }
+         
+
     }
-    return possiblecombinations;
-}
-
-function startGame() {
-    deck.length = 0;
-    //hand.length = 0;
-    selection.length = 0;
-    createDeck();
-    fillHand();
-    document.getElementById("deckSize").innerHTML = "decksize: " + deck.length;
-
-
-    document.getElementById("log").innerHTML = "possible combinations: " + combinations(hand); 
     console.log(hand);
-    
-}
-
-
-//create hasSet function which checks for amount of possible matches in Deck, hand and selection
-
-//start game:createDeck
-//while possible matches in deck != 0
-    //while possible matches in hand != 0
-        //while selection.length != 3
-            //wait for adding cards
-        //isset?
-            //removecards from hand
-    //shuffle
-
-
-    
+    startGameButton.addEventListener('click', startGame);
+    console.log(hand);
+    startGameButton.addEventListener('click', shuffle);
+});
